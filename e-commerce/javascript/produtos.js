4b5d7fb39ce798908afd116e782adcd889dab3c6
mh-candido <candidomatheus617@gmail.com>
2022-12-02 00:06:33 -0300

    first commit

---
+++ e-commerce/javascript/produtos.js
@@ -0,0 +1,60 @@
+const produtos = [
+    {
+        id: 1,
+        image: 'https://images.pexels.com/photos/1332275/pexels-photo-1332275.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Arretado',
+        description: 'Folhas de rúcula, alface americano, lâminas de peito de frango, queijo parmesão regados a um molho especial e só!',
+        value: 20.00
+    },
+    {
+        id: 2,
+        image: 'https://images.pexels.com/photos/1260968/pexels-photo-1260968.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Cabuloso',
+        description: 'Molho de tomate natural, sal e orégano, queijo mussarela, manjericão fresco e só!',
+        value: 35.00
+    },
+    {
+        id: 3,
+        image: 'https://images.pexels.com/photos/4409273/pexels-photo-4409273.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Meu Pirraia',
+        description: 'Pão de gergelim na brasa, blend 150g, queijo mussarela, cebola caramelizada, molho e só!',
+        value: 22.00
+    },
+    {
+        id: 4,
+        image: 'https://images.pexels.com/photos/372886/pexels-photo-372886.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Mai é tu Nada',
+        description: 'Churros de chocolate, com açúcar refinado e só!',
+        value: 7.00
+    },
+    {
+        id: 5,
+        image: 'https://images.pexels.com/photos/2729710/pexels-photo-2729710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Xexeiro',
+        description: 'Batata com rub secreto que leva pimenta, canela, outros ingredientes e só!',
+        value: 12.00
+    },
+    {
+        id: 6,
+        image: 'https://images.pexels.com/photos/7051074/pexels-photo-7051074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Malamanhado',
+        description: 'Massa com suculendo molho a bolonhesa, rechado com creme de ricota, queijo mussarela, parmesão e só!',
+        value: 25.00
+    },
+    {
+        id: 7,
+        image: 'https://images.pexels.com/photos/5809878/pexels-photo-5809878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
+        title: 'Visse?',
+        description: 'Sorvete diversos sabores e só!',
+        value: 5.00
+    },
+    {
+        id: 8,
+        image: 'https://images.pexels.com/photos/2396220/pexels-photo-2396220.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=2000&w=200',
+        title: 'Cangote',
+        description: 'Café expresso com leite e só!',
+        value: 800.00
+    },
+]
+
+
